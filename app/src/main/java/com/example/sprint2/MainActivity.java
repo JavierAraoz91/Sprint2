4ab4adf97fcdef042de5230deca829342aa3ad32
JavierAraoz <114873680+JavierAraoz@users.noreply.github.com>
2022-12-08 23:57:20 -0500

    version1

--- app/src/main/java/com/example/sprint2/MainActivity.java
+++ app/src/main/java/com/example/sprint2/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.sprint2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Context contexto1;
+    Context contexto2;
+    Button btn1;
+    Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1=findViewById(R.id.btnIniciar);
+        btn2=findViewById(R.id.btnRegistrar);
+        contexto1=getApplicationContext();
+        btn1.setOnClickListener(rolActivity);
+        btn2.setOnClickListener(registroActivity);
     }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+    private View.OnClickListener rolActivity=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent int1= new Intent(contexto1, rolActivity.class);
+            startActivity(int1);
+        }
+    };
+    private View.OnClickListener registroActivity=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent int2= new Intent(contexto1, registroActivity.class);
+            startActivity(int2);
+        }
+    };
 }
